58a4544247ecfd923cb8dbe62b6cb82714a8d18d
Tharun Balaji <tharunbalaji110@gmail.com>
2024-12-06 12:56:07 +0530

    added deploy scripts

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import protectRoute from "./middleware/protectRoute.js";
@@ -14,6 +14,9 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages",protectRoute, messageRoutes);
 app.use("/api/users",protectRoute, userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, function () {
